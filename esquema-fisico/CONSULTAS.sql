b749ebc97a9a25251ef3944ad7ede6366bac8369
anac <anacassi.a@outlook.com>
2025-03-13 22:01:55 -0300

    Add ; to querys in CONSULTAS, add some querys

--- esquema-fisico/CONSULTAS.sql
+++ esquema-fisico/CONSULTAS.sql
@@ -3,23 +3,24 @@
 -- Consultas relacionadas a filmes
 
     -- Listar todos os filmes assistidos por 1 usuário
-SELECT f.NOME FROM Filme f INNER JOIN Assistiu a ON a.ID_FILME = f.ID_FILME WHERE a.USERNAME = 'Joao'
+SELECT f.NOME FROM Filme f INNER JOIN Assistiu a ON a.ID_FILME = f.ID_FILME WHERE a.USERNAME = 'Joao';
     -- Listar todos os filmes que 1 usuário quer assistir
-SELECT f.NOME FROM Filme f INNER JOIN Assistira a ON a.ID_FILME = f.ID_FILME WHERE a.USERNAME = 'Joao'
+SELECT f.NOME FROM Filme f INNER JOIN Assistira a ON a.ID_FILME = f.ID_FILME WHERE a.USERNAME = 'Joao';
     -- Listar todos os filmes de 1 gênero
-SELECT f.NOME FROM Filme f WHERE f.GENERO = 'TERROR'
+SELECT f.NOME FROM Filme f WHERE f.GENERO = 'TERROR';
 
 -- Consultas relacionadas a avaliações:
 
     -- Listar todas as avaliações feitas por 1 usuário
     -- Listar todas as avaliações de 1 filme
-SELECT r.USERNAME, r.NOTA, r.DESCRICAO FROM Avaliacao r WHERE r.ID_FILME = 'The Substance'
+SELECT r.USERNAME, r.NOTA, r.DESCRICAO FROM Avaliacao r WHERE r.ID_FILME = 'The Substance';
 
 -- Consultas relacionadas a usuários:
 
 
 -- Consultas relacionadas a solicitações:
-
+    -- Listar todas as solicitações de filmes pendentes de aprovação
+SELECT s.ID_FILME, s.NOME FROM Solicitacao_Filme s LEFT OUTER JOIN Aprova a ON a.ID_FILME = s.ID_FILME;
 -- RELATÓRIOS
 
 -- Filmes mais assistidos por período:
@@ -27,6 +28,7 @@ SELECT r.USERNAME, r.NOTA, r.DESCRICAO FROM Avaliacao r WHERE r.ID_FILME = 'The 
 -- Filmes com melhor avaliação média:
 -- Filmes com pior avaliação média:
 -- Tags mais utilizadas em filmes assistidos:
+SELECT TAG, COUNT(TAG) FROM Tags GROUP BY TAG ORDER BY COUNT(TAG) DESC;
 -- Atores mais presentes em filmes assistidos:
 -- Usuários mais ativos (avaliações, favoritos, assistidos):
 -- Filmes assistidos por idioma:
